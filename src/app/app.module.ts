e42d48e07730d20f86320258321b5bec5bc27f7c
geig006 <matthias.geiger@bertelsmann.de>
2017-09-13 14:42:47 +0200

    initial

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,15 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import {PdfViewerComponent} from 'ng2-pdf-viewer';
+import {FormsModule} from '@angular/forms';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent, PdfViewerComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule, FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
